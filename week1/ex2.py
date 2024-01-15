9222dfe0634b46e768ffe04167ea1cacb893a387
cw-codes <chawakorn.works@gmail.com>
2024-01-15 22:32:34 +0700

    to create a working virtual environment.

---
+++ week1/ex2.py
@@ -0,0 +1,6 @@
+#write a program to find the sum of the weights of three people
+w1 = float(input("Enter weight: "))
+w2 = float(input("Enter weight: "))
+w3 = float(input("Enter weight: "))
+sum = format(w1+w2+w3,'.2f')
+print("Sum of the weights is ",sum)
